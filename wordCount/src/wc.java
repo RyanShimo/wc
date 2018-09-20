ca9e73d72723026fb2358301151b2653cc15c801
RyanShimo <rshimozaki@yahoo.com>
2018-09-19 17:50:29 -0700

    update

--- wordCount/src/wc.java
+++ wordCount/src/wc.java
@@ -8,7 +8,7 @@ import java.io.*;
 
 public class wc 
 {
-	public static void main (String []args)
+	public static void main (String []args) throws FileNotFoundException
 	{
 		if(args[0]=="wc")
 		{
